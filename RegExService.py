eca68957ea62343050af0e49a630cfc8d9c0e4c4
Aditi Ravi <69166723+AditiRavi@users.noreply.github.com>
2022-02-22 03:58:14 +0530

    Initial commit

---
+++ RegExService.py
@@ -0,0 +1,25 @@
+import re
+
+# function using regular expression to reads an return all the data from the dataset
+def getData(fileName):
+    f = open(f"C:\\Users\\ADITI\\PycharmProjects\\FA_VRP\\instances\\P-VRP\\{fileName}","r")
+    content = f.read()
+    trucks = re.search("trucks: (\d+)", content, re.MULTILINE)
+    optimalValue = re.search("Optimal value: (\d+)", content, re.MULTILINE)
+    if(optimalValue != None):
+        optimalValue = optimalValue.group(1)
+    else:
+        optimalValue = re.search("Best value: (\d+)", content, re.MULTILINE)
+        if(optimalValue != None):
+            optimalValue = optimalValue.group(1)
+    dimension=re.search("^DIMENSION : (\d+)$", content, re.MULTILINE).group(1)
+    capacity = re.search("^CAPACITY : (\d+)$", content, re.MULTILINE).group(1)
+    graph = re.findall(r"^(\d+) (\d+) (\d+)$", content, re.MULTILINE)
+    demand = re.findall(r"^(\d+) (\d+)$", content, re.MULTILINE)
+    graph = {int(a):(int(b),int(c)) for a,b,c in graph}
+    demand = {int(a):int(b) for a,b in demand}
+    capacity = int(capacity)
+    trucks = trucks.group(1)
+    trucks=int(trucks)
+    optimalValue = int(optimalValue)
+    return capacity, graph, demand, optimalValue, trucks,dimension
